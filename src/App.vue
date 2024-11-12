5b7cbf26cbe97f53eb3e6e20d2b7fda45a1ab838
ArsalGitHub <arsal3414@gmail.com>
2024-11-12 21:43:27 +0500

    set the new component and its route

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,8 @@
 
 <template>
 
-<h1 class="text-3xl font-bold underline bg-amber-300">
-    Hello world!
-  </h1>
+
+<router-view></router-view>
 </template>
 
 <style scoped>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="text-3xl font-bold underline bg-amber-300">
+    Hello world! from home
+  </h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
