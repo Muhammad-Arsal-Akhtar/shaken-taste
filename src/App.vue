e23384f90cafff61bab395a5c7394a0e8c1f62c0
ArsalGitHub <arsal3414@gmail.com>
2024-11-14 23:29:23 +0500

    set the layout for the APP component

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,31 @@
 
 <template>
 
+    <div class="h-full bg-gray-200">
+        <header class="flex items-stretch justify-between bg-white shadow-md h-14">
+            <router-link :to="{ name: 'home' }" class="inline-flex items-center h-full px-5 hover:bg-green-200">
+                Home
+            </router-link>
 
-<router-view></router-view>
+            <div class="flex items-center gap-1">
+                <router-link to="/" class="inline-flex items-center h-full px-2 hover:bg-green-200">
+                    Search cocktails
+                </router-link>
+
+                <router-link to="/" class="inline-flex items-center h-full px-2 hover:bg-green-200">
+                    Cocktails By Letter
+                </router-link>
+
+                <router-link to="/" class="inline-flex items-center h-full px-2 hover:bg-green-200">
+                    Search By Ingredients
+                </router-link>
+            </div>
+        </header>
+
+        <main>
+            <router-view></router-view>
+        </main>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
