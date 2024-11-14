1a146da138d36ec0ab43be1097f59649a348e32a
ArsalGitHub <arsal3414@gmail.com>
2024-11-14 21:38:00 +0500

    set the imput and search by letter html

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,19 @@
 <template>
-    <h1 class="text-3xl font-bold underline bg-amber-300">
+    <!-- <h1 class="text-3xl font-bold underline bg-amber-300">
     Hello world! from home {{ juices }}
-  </h1>
+  </h1> -->
+  <div class="flex flex-col p-8">
+    <div>
+      <input type="text" class="w-full border-2 border-gray-300 rounded" placeholder="Enter the drink name..." />
+    </div>
+
+    <div class="flex justify-center gap-1 mt-2">
+      <router-link to="/" v-for="letter in letters">
+        {{ letter }}
+      </router-link>
+    </div>
+      
+  </div>
 </template>
 
 <script setup>
@@ -10,6 +22,7 @@ import { useCocktailStore } from '@/stores/cocktail'
 const cocktailStore = useCocktailStore()
 const {juices} = cocktailStore
 
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 </script>
