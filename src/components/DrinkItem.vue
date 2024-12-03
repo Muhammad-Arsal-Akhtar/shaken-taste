83efa0ff41c0bc9ce2bce79fc38987d1da6b7e16
ArsalGitHub <arsal3414@gmail.com>
2024-12-03 23:03:46 +0500

    make the separate component for the card of the drinks item

---
+++ src/components/DrinkItem.vue
@@ -0,0 +1,28 @@
+<template>
+      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <router-link :to="{ name: 'drinkDetails', params: { id : drink.id } }">
+        <img class="rounded-t-lg w-[383px] h-[255px]" :src="drink.image" :alt="drink.imageAlt" />
+      </router-link>
+      <div class="p-5">
+        <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{drink.name}}</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+          acquisitions of 2021 so far, in reverse chronological order.</p>
+        <VideoButton :href="drink.video" />
+      </div>
+    </div>
+
+</template>
+
+<script setup>
+import VideoButton from '@/components/VideoButton.vue';
+
+defineProps({
+  drink: {
+    require : true,
+    type : Object
+  },
+})
+
+</script>
--- src/views/CocktailByNameView.vue
+++ src/views/CocktailByNameView.vue
@@ -6,19 +6,7 @@
 
 
   <div class="grid grid-cols-1 gap-3 p-8 md:grid-cols-3">
-    <div v-for="drink in drinks" :key="drink.id" class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <router-link :to="{ name: 'drinkDetails', params: { id : drink.id } }">
-        <img class="rounded-t-lg w-[383px] h-[255px]" :src="drink.image" :alt="drink.imageAlt" />
-      </router-link>
-      <div class="p-5">
-        <a href="#">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{drink.name}}</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
-          acquisitions of 2021 so far, in reverse chronological order.</p>
-        <VideoButton :href="drink.video" />
-      </div>
-    </div>
+    <DrinkItem v-for="drink in drinks" :key="drink.id" :drink="drink"  />
   </div>
 
 </template>
@@ -27,7 +15,7 @@
 import { useSearchDrinksStore } from '@/stores/searchDrinks';
 import { ref, computed, onMounted } from 'vue';
 import { useRoute } from 'vue-router'
-import VideoButton from '@/components/VideoButton.vue';
+import DrinkItem from '@/components/DrinkItem.vue';
 
 const drinksStore = useSearchDrinksStore()
 const route = useRoute()
