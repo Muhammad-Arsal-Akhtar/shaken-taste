717b999a0e22177271f6f9d976737f8356a0b2cf
ArsalGitHub <arsal3414@gmail.com>
2024-12-10 20:46:19 +0500

    changes the youtube button to green

--- src/components/VideoButton.vue
+++ src/components/VideoButton.vue
@@ -1,6 +1,6 @@
 <template>
 <a :href="props.href"
-          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 hover:cursor-pointer" target="_blank">
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 hover:cursor-pointer" target="_blank">
           Video
           <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
             viewBox="0 0 14 10">
