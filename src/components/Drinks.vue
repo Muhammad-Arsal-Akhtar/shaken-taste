d0d6327f2c110437088b1392e390d2aba1582712
ArsalGitHub <arsal3414@gmail.com>
2024-12-13 12:21:12 +0500

    adjusted the card according to the screen

--- src/components/Drinks.vue
+++ src/components/Drinks.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="grid grid-cols-1 gap-3 p-8 md:grid-cols-3">
+    <div class="grid min-h-screen grid-cols-1 gap-3 p-8 sm:grid-cols-2 md:grid-cols-3 place-items-center">
       <DrinkItem v-for="drink in drinks" :key="drink.id" :drink="drink" />
     </div>
-
-
+    
     <div class="flex justify-center" v-if="!drinks.length">
         <span>
             There are no drinks available.
