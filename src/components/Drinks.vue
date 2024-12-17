6a4d30888e5bbfdac0f4f862ddd95e342dc2b917
ArsalGitHub <arsal3414@gmail.com>
2024-12-17 13:22:36 +0500

    set the card and screen for the search by namme cock tail

--- src/components/Drinks.vue
+++ src/components/Drinks.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="grid min-h-screen grid-cols-1 gap-3 p-8 sm:grid-cols-2 md:grid-cols-3 place-items-center">
-      <DrinkItem v-for="drink in drinks" :key="drink.id" :drink="drink" />
+    <div class="grid grid-cols-1 gap-3 p-8 sm:grid-cols-2 md:grid-cols-3 place-items-center">
+        <DrinkItem v-for="drink in drinks" :key="drink.id" :drink="drink" />
     </div>
-    
+
     <div class="flex justify-center" v-if="!drinks.length">
-        <span>
-            There are no drinks available.
-        </span>
+        <a href="#"
+            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">There are no drinks
+                available. Type on the Search to get.</h5>
+        </a>
     </div>
 </template>
 
@@ -15,9 +17,9 @@
 import DrinkItem from './DrinkItem.vue';
 
 defineProps({
-    drinks : {
-        require : true,
-        type : Object
+    drinks: {
+        require: true,
+        type: Object
     }
 })
 
